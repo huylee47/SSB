3b89fa1b1542906a7ae3b0f7967ce70887137807
huylee47 <huyw300@gmail.com>
2024-12-26 08:39:51 +0700

    view , etc

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DataTable - Mazer Admin Dashboard</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ url('') }}/assets/css/bootstrap.css">
+
+    <link rel="stylesheet" href="{{ url('') }}/assets/css/vendors/simple-datatables/style.css">
+    <link rel="stylesheet" href="{{ url('') }}/assets/css/vendors/iconly/bold.css">
+
+    <link rel="stylesheet" href="{{ url('') }}/assets/css/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="{{ url('') }}/assets/css/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{ url('') }}/assets/css/app.css">
+    {{-- <link rel="shortcut icon" href="{{ url('') }}/assets/images/favicon.svg" type="image/x-icon"> --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select/dist/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css">
+
+    <!-- Thêm CSS từ section 'styles' nếu có -->
+    @yield('styles')
+</head>
+
+<body>
+    <div id="app">
+        <div id="sidebar" class="active">
+            <div class="sidebar-wrapper active">
+                <div class="sidebar-header">
+                    <div class="d-flex justify-content-between">
+                        <div class="logo">
+                            <a href="index.html"><img src="{{ url('') }}/assets/img/logo/logo.png"
+                                    alt="Logo" srcset=""></a>
+                        </div>
+                        <div class="toggler">
+                            <a href="#" class="sidebar-hide d-xl-none d-block"><i
+                                    class="bi bi-x bi-middle"></i></a>
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="sidebar-menu">
+                    <ul class="menu">
+                        <li class="sidebar-title">Menu</li>
+
+                        <li class="sidebar-item  {{ request()->routeIs('index') ? 'active' : '' }}">
+                            <a href="{{ route('index') }}" class='sidebar-link'>
+                                <i class="bi bi-grid-fill"></i>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+
+                        <li class="sidebar-item {{ request()->routeIs('admin.project.index') ? 'active' : '' }}">
+                            <a href="{{ route('admin.project.index') }}" class='sidebar-link'>
+                                <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                                <span>Dự án</span>
+                            </a>
+                        </li>
+
+                        <li class="sidebar-item {{ request()->routeIs('admin.property.index') ? 'active' : '' }}">
+                            <a href="{{ route('admin.property.index') }}" class='sidebar-link'>
+                                <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                                <span>Bất động sản</span>
+                            </a>
+                        </li>
+
+                        <li class="sidebar-item {{ request()->routeIs('admin.contact.index') ? 'active' : '' }}">
+                            <a href="{{ route('admin.contact.index') }}" class='sidebar-link'>
+                                <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                                <span>Liên hệ</span>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div> --}}
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+            </div>
+        </div>
+        @yield('main')
+        <footer>
+            <div class="footer clearfix mb-0 text-muted">
+                <div class="float-start">
+                    <p>2021 &copy; Mazer</p>
+                </div>
+                <div class="float-end">
+                    <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                            href="http://ahmadsaugi.com">A. Saugi</a></p>
+                </div>
+            </div>
+        </footer>
+    </div>
+    </div>
+    <script src="{{ url('') }}/assets/css/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="{{ url('') }}/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ url('') }}/assets/css/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="{{ url('') }}/assets/css/vendors/apexcharts/apexcharts.js"></script>
+    <script src="{{ url('') }}/assets/css/vendors/jquery/jquery.min.js"></script>
+    <script src="{{ url('') }}/assets/css/vendors/simple-datatables/simple-datatables.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
+
+    <script>
+        // Simple Datatable
+        let table1 = document.querySelector('#table1');
+        let dataTable = new simpleDatatables.DataTable(table1);
+    </script>
+
+    <script src="{{ url('') }}/assets/js/main.js"></script>
+
+    <!-- Thêm script JS từ section 'scripts' nếu có -->
+    @yield('scripts')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('client.layouts.index');
+});
+Route::get('/admin', function () {
+    return view('admin.dashboard.index');
 });
