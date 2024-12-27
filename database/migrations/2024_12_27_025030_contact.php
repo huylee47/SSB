06ce00bf42a5068809a5a78ab7bd69c1e5fdb108
huylee47 <huyw300@gmail.com>
2024-12-27 09:52:28 +0700

    base

--- database/migrations/2024_12_27_025030_contact.php
+++ database/migrations/2024_12_27_025030_contact.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('contacts',function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->text('message');
             $table->timestamps();
         });
     }
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('contacts');
     }
 };
