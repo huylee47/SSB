c38edd4678dd50b927be7fb3f468e88355c90da1
huylee47 <huyw300@gmail.com>
2024-12-30 11:29:43 +0700

    Course srvc

--- app/Service/CourseService.php
+++ app/Service/CourseService.php
@@ -3,6 +3,8 @@
 namespace App\Service;
 use App\Http\Resources\CourseResource;
 use App\Models\Course;
+use Illuminate\Http\Request;
+
 class CourseService {
     public function index(){
         $courses = Course::all();
@@ -17,5 +19,51 @@ class CourseService {
         $courses =  CourseResource::collection($courses);
         return $courses;
     }
-
+    public function create(){
+        return view('admin.course.add');
+    }
+    public function store(Request $request)
+    {
+    
+        $imageName = null;
+        if ($request->hasFile('thumbnail')) {
+            $imageName = time() . '_' . uniqid() . '.' . $request->thumbnail->getClientOriginalExtension();
+            $request->thumbnail->move(public_path('assets/img/thumbnails'), $imageName);
+        }
+    
+        // Lưu khóa học
+        Course::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'content' => $request->content,
+            'thumbnail' => $imageName,
+        ]);
+    
+        return redirect()->route('admin.course.index')->with('success', 'Thêm khóa học thành công!');
+    }
+    
+    public function edit($id){
+        $course = Course::find($id);
+        return view('admin.course.edit',compact('course'));
+    }
+    
+    public function update(Request $request, $id){
+        $course = Course::find($id);
+        $course->title = $request->title;
+        $course->description = $request->description;
+        $course->content = $request->content;
+        if ($request->hasFile('thumbnail')) {
+            $imageName = time(). '_'. uniqid(). '.'. $request->thumbnail->getClientOriginalExtension();
+            $request->thumbnail->move(public_path('assets/img/thumbnails'), $imageName);
+            $course->thumbnail = $imageName;
+        }
+        $course->save();
+        return redirect()->route('admin.course.index')->with('success', 'Cập nhật khóa học thành công!');
+    }
+    public function delete($id){
+        $course = Course::find($id);
+        $course->delete();
+        return redirect()->route('admin.course.index')->with('success', 'Xóa khóa học thành công!');
+    }
+    
 }
