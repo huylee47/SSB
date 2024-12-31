d4304df24d95964218cb62891b44fbd8ec29b40c
huylee47 <huyw300@gmail.com>
2024-12-31 14:16:43 +0700

    view

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DataTable - Mazer Admin Dashboard</title>
+    <title>SSB | Trang quản trị</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
@@ -90,12 +90,12 @@
         <footer>
             <div class="footer clearfix mb-0 text-muted">
                 <div class="float-start">
-                    <p>2021 &copy; Mazer</p>
+                    <p>2024 &copy; OuranSoft</p>
                 </div>
-                <div class="float-end">
+                {{-- <div class="float-end">
                     <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
                             href="http://ahmadsaugi.com">A. Saugi</a></p>
-                </div>
+                </div> --}}
             </div>
         </footer>
     </div>
