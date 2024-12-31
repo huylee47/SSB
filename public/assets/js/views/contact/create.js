39ea09c87c98890c90e1b62ea1a7221286da9e2e
huyv02961 <huyvhpp02961@fpt.edu.vn>
2024-12-31 14:13:11 +0700

    Contact Form

---
+++ public/assets/js/views/contact/create.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('#contactForm').on('submit', function (e) {
+        e.preventDefault();
+
+        // Disable the submit button to prevent multiple clicks
+        const submitButton = $(this).find(':submit');
+        submitButton.prop('disabled', true);
+
+        $.ajax({
+            type: 'POST',
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            success: function (response) {
+                Swal.fire('Thành công', 'Tin nhắn của bạn đã được gửi đi!', 'success').then(function () {
+                    $('#contactForm')[0].reset();
+                    submitButton.prop('disabled', false); // Re-enable button after SweetAlert
+                });
+            },
+            error: function (xhr) {
+                let errorMessage = 'Lỗi gửi tin nhắn';
+                if (xhr.status === 409) {
+                    errorMessage = 'Email đã gửi đi trước đó!';
+                }
+
+                Swal.fire('Error', errorMessage, 'error').then(function () {
+                    submitButton.prop('disabled', false); // Re-enable button after SweetAlert
+                });
+            }
+        });
+    });
+});
