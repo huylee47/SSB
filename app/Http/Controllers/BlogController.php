8905da47e249c4ca14f3d1baf4b1bafa097af437
huylee47 <huyw300@gmail.com>
2025-01-02 11:44:28 +0700

    Make Blog Ctrl

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
+use App\Service\BlogService;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
+    private $blogService;
+    public function __construct(BlogService $blogService){
+        $this->blogService = $blogService;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return $this->blogService->index();
     }
 
     /**
@@ -20,7 +25,7 @@ class BlogController extends Controller
      */
     public function create()
     {
-        //
+        return $this->blogService->create();
     }
 
     /**
@@ -28,38 +33,37 @@ class BlogController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return $this->blogService->store($request);
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Blog $blog)
-    {
-        //
-    }
-
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Blog $blog)
+    public function edit($id)
     {
-        //
+        return $this->blogService->edit($id);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Blog $blog)
+    public function update(Request $request, $id)
     {
-        //
+        return $this->blogService->update($request, $id);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Blog $blog)
+    public function delete($id)
     {
-        //
+        return $this->blogService->delete($id);
+    }
+    // CLient 
+        /**
+     * Display the specified resource.
+     */
+    public function show($slug)
+    {
+        return $this->blogService->show($slug);
     }
 }
