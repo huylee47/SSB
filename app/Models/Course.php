d86bbba7534b5b0e75aec069627ccd79f89587c1
huylee47 <huyw300@gmail.com>
2025-01-03 11:44:27 +0700

    udate course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,10 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+
 
 class Course extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
     protected $table = 'courses';
-    protected $fillable = ['title', 'description', 'thumbnail', 'content'];
+    protected $fillable = ['title', 'description', 'thumbnail', 'content','slug'];
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($course) {
+            if (empty($course->slug)) {
+                $slug = Str::slug($course->title);
+                $count = Course::where('slug', 'like', "{$slug}%")->count();
+                $course->slug = $count > 0 ? "{$slug}-" . ($count + 1) : $slug;
+            }
+        });
+    }
 }
