e8da0eb8ba139a3dbc2ec089147c6ddc45ab45e2
huylee47 <huyw300@gmail.com>
2025-01-03 12:00:48 +0700

    update migration course

--- database/migrations/2024_12_27_021925_course.php
+++ database/migrations/2024_12_27_021925_course.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->text('description');
             $table->text('content');
             $table->string('thumbnail');
+            $table->string('slug')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
