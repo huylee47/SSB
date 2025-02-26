608997d7ed5da59cdc9617ca7cae7b9194244b9c
HuyLee <huyw300@gmail.com>
2025-02-26 21:27:43 +0700

    set url css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SSB | Đăng nhập </title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/vendors/bootstrap-icons/bootstrap-icons.css">
-    <link rel="stylesheet" href="assets/css/app.css">
-    <link rel="stylesheet" href="assets/css/auth.css">
+    <link rel="stylesheet" href="{{ url('') }}/assets/css/bootstrap.css">
+    <link rel="stylesheet" href="{{ url('') }}/assets/css/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{ url('') }}/assets/css/app.css">
+    <link rel="stylesheet" href="{{ url('') }}/assets/css/auth.css">
 </head>
 
 <body>
